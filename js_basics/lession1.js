093b90fc788c03d4597f136105738ca5c6608a6d
tapati <tapatikol07@gmail.com>
2026-07-01 05:39:43 +0000

    data type & conversion

--- js_basics/lession1.js
+++ js_basics/lession1.js
@@ -1,1 +1,41 @@
-console.log("Tapati")
+//console.log("Tapati")
+
+let name= "Tapati"
+const email ="email@gmail.com"
+let phone = 1234567890
+
+name = "Pampa"
+//email = "pampa@gmail.com"
+phone = 8762345
+
+//console.table([name,email,phone])
+
+//Data types
+
+let enrollment_number = "20261012qwe"
+let my_name = "Tapati123"
+let isLogIn = 1
+let address = " "
+let car ="null";
+
+//console.log(typeof enrollment_number)
+//console.log(typeof my_name)
+//console.log(typeof isLogIn)
+//console.log(typeof address)
+//console.log(typeof car)
+
+//Conversion
+
+let new_num = Number(enrollment_number)
+//console.log(typeof new_num)
+//console.log(new_num)
+
+let new_name = String(my_name)
+//console.log(typeof new_name)
+//console.log(new_name)
+
+let loggedIn = Boolean(isLogIn)
+
+//console.log(loggedIn)
+
+
